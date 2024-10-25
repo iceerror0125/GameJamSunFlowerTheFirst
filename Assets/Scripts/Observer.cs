f7b6257596444c3450d8a75b435f63e64e8c7a60
unknown <truongtrangiahung@gmail.com>
2024-10-26 00:26:35 +0700

    init

---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+
+public class Message
+{
+    public EventType eventType;
+    public object param;
+    public object returnValue;
+
+    public Message(EventType eventType, object param = null)
+    {
+        this.eventType = eventType;
+        this.param = param;
+    }
+    public Message(object param)
+    {
+        this.param = param;
+    }
+}
+
+public class Observer : Singleton<Observer>
+{
+    private Dictionary<EventType, List<Action<Message>>> observer = new();
+
+    public void Subscribe(EventType type, Action<Message> callBack)
+    {
+        if (observer.ContainsKey(type))
+        {
+            observer[type].Add(callBack);
+        }
+        else
+        {
+            observer.Add(type, new List<Action<Message>> { callBack });
+        }
+    }
+    public void UnSubscribe(EventType type, Action<Message> callBack)
+    {
+        if (observer.ContainsKey(type))
+        {
+            if (observer[type].Count == 1)
+            {
+                observer.Remove(type);
+            }
+            else
+            {
+                observer[type].Remove(callBack);
+            }
+        }
+        else
+        {
+            Debug.LogError("Unsubscribe fail: Event type doesn't exist");
+        }
+    }
+    public void Announce(Message message)
+    {
+        EventType type = message.eventType;
+        if (observer.ContainsKey(type))
+        {
+            foreach (Action<Message> action in observer[type])
+            {
+                action?.Invoke(message);
+            }
+        }
+        else
+        {
+            Debug.LogError("Announce fail: Event type doesn't exist");
+        }
+    }
+
+
+    public object GetCallBack(Message callBackMsg)
+    {
+        Announce(callBackMsg);
+        return callBackMsg.returnValue;
+    }
+}
